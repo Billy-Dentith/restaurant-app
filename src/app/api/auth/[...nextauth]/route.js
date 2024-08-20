bf3f6588865dd368eefa292b3a427aea2895076d
Billy Dentith <billydentith@hotmail.co.uk>
2024-08-20 16:46:51 +0100

    Adds next auth connection to database

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,20 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "pg"
+
+const pool = new Pool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
 
 export const authOptions = {
+    adapter: PostgresAdapter(pool), 
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
