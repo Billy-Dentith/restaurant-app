e1af6bad44e9b9611e86a0f83d61af73712be708
Billy Dentith <billydentith@hotmail.co.uk>
2024-11-01 23:56:09 +0000

    Modified orders page to contain order creation time and adjusted styling

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -59,8 +59,8 @@ const OrdersPage = () => {
     if (isLoading || status === "loading") return "Loading..."
 
     return (
-        <div className="p-4 lg:px-20 xl:px-40">
-            <table className="w-full border-separate border-spacing-3">
+        <div className="pt-4 p-2 lg:px-20 xl:px-40">
+            <table className="w-full border-separate border-spacing-2">
                 <thead>
                     <tr className="text-left">
                         <th className="hidden md:block">Order ID</th>
@@ -68,30 +68,37 @@ const OrdersPage = () => {
                         <th>Price</th>
                         <th className="hidden md:block">Products</th>
                         <th>Status</th>
+                        <th>Remove</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.orders.map((item) => {
+                        const date = item.created_at.replace('T', ' ').substring(0, 16)
+
                         return (
-                        <tr className={`${item.status !== "completed" && "bg-red-100"}`} key={item.id}>
-                            <td className="hidden md:block py-6 px-1">{item.id}</td>
-                            <td className="py-6 px-1">22/07/2024</td>
-                            <td className="py-6 px-1">{formatPrice(item.price)}</td>
-                            <td className="hidden md:block py-6 px-1">{item.products.quantity}</td>
+                        <tr className={`${item.status === "completed" ? "bg-green-100" : "bg-red-100"}`} key={item.id}>
+                            <td className="hidden md:block py-8 px-2">{item.id}</td>
+                            <td className="py-6 px-2">{date.split(' ')[0].split('-').reverse().join('-')} {date.split(' ')[1]}</td>
+                            <td className="py-6 px-2">{formatPrice(item.price)}</td>
+                            <td className="hidden md:block py-8 px-2">{item.products.quantity}</td>
                             {session.is_admin ? (
                                     <td>
-                                        <form className="flex items-center justify-center gap-4" onSubmit={(e) => handleUpdate(e, item.id)}>
-                                            <input placeholder={item.status} className="p-2 ring-1 ring-red-100 rounded-md"/>
+                                        <form className="flex flex-col md:flex-row items-center justify-center gap-2 px-1" onSubmit={(e) => handleUpdate(e, item.id)}>
+                                            <input placeholder={item.status} className="p-2 ring-1 ring-red-100 rounded-md w-24 md:w-auto"/>
                                             <button className="bg-red-400 p-2 rounded-full">
-                                                <Image src="/edit.png" alt="" width={20} height={20} 
+                                                <Image src="/edit.png" alt="edit order" width={20} height={20} 
                                                 />
                                             </button>
                                         </form>
                                     </td>
                                 ) : (
-                                    <td className="py-6 px-1">{item.status}</td>
-                                )}
-                                {/* <td className="py-6 px-1">{item.status}</td> */}
+                                    <td className="py-6 px-2">{item.status}</td>
+                            )}
+                            <td className="py-6 px-2">
+                                <button className="bg-red-400 flex justify-self-center p-2 rounded-full">
+                                    <Image src="/delete.png" alt="remove order" width={20} height={20} />
+                                </button>
+                            </td>
                         </tr>
                         )
                     })}
