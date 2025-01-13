75a188a8b71851a2fadb2b208079c0fa7b09ac7c
Billy Dentith <billydentith@hotmail.co.uk>
2025-01-13 23:19:09 +0000

    Amends payment successful page

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
@@ -21,29 +21,49 @@ const data = [
   },
 ];
 
-export const Slider = () => {
+export const Slider = ({ imageOnly }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-        setCurrentSlide(prev => (prev === data.length - 1 ? 0 : prev + 1))
-    }, 5000)
+      setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1));
+    }, 5000);
     return () => clearInterval(interval);
-  }, [])
+  }, []);
 
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
-      {/* TEXT CONTAINER */}
-      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className="text-5xl text-center uppercase p-5 md:p-10 md:text-6xl xl:text-7xl">
-          {data[currentSlide].title}
-        </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
-      </div>
-      {/* IMAGE CONTAINER */}
-      <div className="w-full flex-1 relative">
-        <Image src={data[currentSlide].image} alt="" fill className="object-cover" />
-      </div>
-    </div>
+    <>
+      {imageOnly ? (
+        <div className="flex self-center">
+          <Image
+            src={data[currentSlide].image}
+            alt=""
+            height={1000}
+            width={1000}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
+          {/* TEXT CONTAINER */}
+          <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+            <h1 className="text-5xl text-center uppercase p-5 md:p-10 md:text-6xl xl:text-7xl">
+              {data[currentSlide].title}
+            </h1>
+            <button className="bg-red-500 text-white py-4 px-8">
+              Order Now
+            </button>
+          </div>
+          {/* IMAGE CONTAINER */}
+          <div className="w-full flex-1 relative">
+            <Image
+              src={data[currentSlide].image}
+              alt=""
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
