a962e2cfbe0937386b4b91f9aff731dd612d2579
Billy Dentith <billydentith@hotmail.co.uk>
2025-01-31 22:47:16 +0000

    Amends Featured component to render products that have is_featured set to true

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,32 +1,69 @@
-import { featuredProducts } from "@/data";
+import { formatPrice } from "@/utils/formatPrice";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-export const Featured = () => {
+const getProducts = async () => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/products?isFeatured=true`,
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch products");
+    }
+
+    const { products } = await response.json();
+    return products;
+  } catch (error) {
+    console.error("Failed to fetch products:", error.message);
+    throw error;
+  }
+};
+
+export const Featured = async () => {
+  const products = await getProducts();
+
   return (
     <div className="w-100vw overflow-x-scroll text-red-500">
       {/* WRAPPER */}
       <div className="w-max flex">
         {/* SINGLE ITEM */}
-        {featuredProducts.map((item) => (
+        {products.map((item) => (
           <div
             key={item.id}
             className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w=[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINER */}
-            {item.img && (
+            {item.image && (
               <div className="relative flex-1 w-full hover:rotate-45 transition-all duration-500">
-                <Image src={item.img} alt="" fill className="object-contain " />
+                <Image
+                  src={item.image}
+                  alt=""
+                  fill
+                  className="object-contain"
+                />
               </div>
             )}
             {/* TEXT CONTAINER */}
             <div className="flex-1 flex flex-col items-center justify-center text-center gap-4">
-              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
-              <p className="p-4 2xl:p-8">{item.desc}</p>
-              <span className="text-xl font-bold">£{item.price}</span>
-              <button className="bg-red-500 text-white p-2 rounded-md">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {item.title}
+              </h1>
+              <p className="p-4 2xl:p-8">{item.description}</p>
+              <span className="text-xl font-bold">
+                {formatPrice(item.price)}
+              </span>
+              <Link
+                href={`/products/${item.id}`}
+                key={item.id}
+                className="bg-red-500 text-white p-2 rounded-md"
+              >
                 Add to Cart
-              </button>
+              </Link>
             </div>
           </div>
         ))}
